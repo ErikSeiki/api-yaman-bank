e73f0e078669c31af71f85f2ba91ad6e56aa930b
Erik Nishikawa <erik.nishikawa@yaman.com.br>
2020-02-27 09:58:46 -0300

    primeiro commit

---
+++ src/main/java/br/com/yamanbanking/ApiYamanBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.yamanbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiYamanBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiYamanBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/yamanbanking/ApiYamanBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yamanbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiYamanBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
